7ff0e44020164541aa67d2e6fbbe6e131f0d0ae9
Damian Stępień <damian.stepien@me.com>
2019-12-28 00:47:21 +0100

    Make CSS templates more readable by introducing new Twig functions

--- src/Bridge/TwigBridge.php
+++ src/Bridge/TwigBridge.php
@@ -5,6 +5,8 @@ namespace C3\Bridge;
 use C3\Exception\TwigException;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
+use Twig\TwigFunction;
+use Twig\TwigTest;
 
 /**
  * TwigBridge class manages initialization of Twig Environment. It also allows to provide already existing Environment,
@@ -28,24 +30,29 @@ class TwigBridge
     }
 
     /**
+     * @param Environment|null $environment
      * @return void
      * @throws TwigException
      */
-    public static function init(): void
+    public static function init(?Environment $environment = null): void
     {
-        if(self::$environment === null) {
-            if(!self::exists()) {
-                throw new TwigException("Twig not found");
-            }
-
-            $varPath = realpath(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'var');
-
-            self::$environment = new Environment(
-                new FilesystemLoader($varPath . DIRECTORY_SEPARATOR . 'templates')
-            );
+        if(!self::exists()) {
+            throw new TwigException("Twig not found");
         }
 
-        // TODO: init functions etc
+        $varPath = realpath(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'var');
+
+        self::$environment = new Environment(
+            new FilesystemLoader($varPath . DIRECTORY_SEPARATOR . 'templates')
+        );
+
+        self::registerExtensions();
+    }
+
+    public static function setExternalEnvironment(Environment $environment)
+    {
+        self::$environment = $environment;
+        self::registerExtensions();
     }
 
     /**
@@ -59,5 +66,43 @@ class TwigBridge
         }
 
         return self::$environment;
+    }
+
+    /**
+     * @return void
+     */
+    private static function registerExtensions(): void
+    {
+        self::$environment->addFunction(new TwigFunction('_css', function(string $selector, $style) {
+            if(is_array($style)) {
+                $styles = [];
+
+                foreach($style as $key => $value) {
+                    if(is_numeric($key)) {
+                        $styles[] = $value;
+                    } else {
+                        $styles[] = sprintf("%s: %s", $key, $value);
+                    }
+                }
+
+                $styleString = implode('; ', $styles);
+            } else {
+                $styleString = $style;
+            }
+
+            return sprintf("%s { %s }", $selector, $styleString);
+        }));
+
+        self::$environment->addTest(
+            new TwigTest('_modified', function($value, string $className, string $fieldName) {
+                $properties = (new \ReflectionClass($className))->getDefaultProperties();
+
+                if(!isset($properties[$fieldName])) {
+                    return false;
+                }
+
+                return $properties[$fieldName] !== $value;
+            })
+        );
     }
 }
